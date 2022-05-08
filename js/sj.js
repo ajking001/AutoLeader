b47cd34e3d61b4a77b814a6293db4c40ecde8632
ajking001 <95129750+ajking001@users.noreply.github.com>
2022-05-08 18:04:35 +0500

    change header

---
+++ js/sj.js
@@ -0,0 +1,12 @@
+let info = document.querySelector(".info")
+let header_box = document.querySelector(".header_box")
+let close_info = document.querySelector(".close_info")
+
+info.addEventListener("click", () => {
+    header_box.classList.add("open_info")
+
+})
+
+close_info.addEventListener("click", () => {
+    header_box.classList.remove("open_info")
+})
