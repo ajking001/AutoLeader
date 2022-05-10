3ed472477006ec289727325d3d5a693dad65b7c0
ajking001 <95129750+ajking001@users.noreply.github.com>
2022-05-10 16:30:44 +0500

    f

---
+++ js/sj.js
@@ -0,0 +1,6 @@
+let feature-car-rent-box-1 = document.querySelector(".feature-car-rent-box-1")
+let info = document.querySelector(".info")
+
+info.addEventListener("click", () => {
+    feature-car-rent-box-1.classList.add("open_info")
+})
