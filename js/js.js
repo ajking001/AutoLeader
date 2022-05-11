d5e52135cbf2232d64799aaab2257d644d391d4d
karimovvoff <102304060+sanjarkarimovoff@users.noreply.github.com>
2022-05-11 07:15:06 +0500

    2js
    2js

--- js/js.js
+++ js/js.js
@@ -65,3 +65,16 @@ close_info.addEventListener("click", () => {
 //     );
 //   });
 // });
+
+
+// car mark js 
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
