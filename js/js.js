dbde8c9d509e27076c15e2ac935281b914373919
karimovof <102304060+karimovof@users.noreply.github.com>
2022-05-17 10:39:08 +0500

    pegas
    pages

--- js/js.js
+++ js/js.js
@@ -10,6 +10,14 @@ close_info.addEventListener("click", () => {
 })
 
 
+// burger
+let btn = $('.btn');
+
+btn.on('click', function() {
+  $(this).toggleClass('active');
+  $(this).toggleClass('not-active');
+});
+
 //carousel header 
 // let navLinks = document.querySelectorAll(".carousel .nav-link");
 // let slides = document.querySelectorAll(".carousel .slides img");
